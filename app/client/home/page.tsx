870d45330e87ed59fe88a53b15f764705ff4d2bd
Berdyshev Kerey <berdyshev_k1004@akb.nis.edu.kz>
2024-10-18 17:23:46 +0500

    navbar

---
+++ app/client/home/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import React from "react";
+import Navbar from "@/app/components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Navbar/>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,5 +12,8 @@ const config: Config = {
   plugins: [
     require("daisyui"),
   ],
+  daisyui: {
+    themes: ["light"],
+  },
 };
 export default config;
